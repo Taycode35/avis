f116f518cbc49bfb2d07a9c58c2357d7bc33d28e
RIAMA44 <maria.mputu44100@gmail.com>
2022-01-21 09:32:26 +0100

    Ajout des auteurs dans la classe Genre.java

--- src/main/java/com/projetavisG3/avis/business/Genre.java
+++ src/main/java/com/projetavisG3/avis/business/Genre.java
@@ -5,33 +5,30 @@ package com.projetavisG3.avis.business;
 
 /**
  * @author Administrator
- *
+ * @author Andrick, Audrey et Maria
  */
+
 public class Genre {
 	private Long id;
 	private String nom;
-	
-	
-public Genre() {
-	// TODO Auto-generated constructor stub
-}
 
+	public Genre() {
+		// TODO Auto-generated constructor stub
+	}
 
-/**
- * @param id
- * @param nom
- */
-public Genre(Long id, String nom) {
-	super();
-	this.id = id;
-	this.nom = nom;
-}
+	/**
+	 * @param id
+	 * @param nom
+	 */
+	public Genre(Long id, String nom) {
+		super();
+		this.id = id;
+		this.nom = nom;
+	}
 
-
-@Override
-public String toString() {
-	return "Genre [id=" + id + ", nom=" + nom + "]";
-}
-
+	@Override
+	public String toString() {
+		return "Genre [id=" + id + ", nom=" + nom + "]";
+	}
 
 }
